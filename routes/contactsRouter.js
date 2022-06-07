80f29a9ba1911df4a7b839292359175f401d3865
Janette Campbell <campbjp@hotmail.com>
2022-06-07 12:50:31 -0400

    added /contacts routes

---
+++ routes/contactsRouter.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const ContactModel = require("../models/contactsSchema");
+
+const router = express.Router();
+
+// Get contacts
+router.get("/", async (req, res) => {
+  try {
+    const contacts = await ContactModel.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Create contacts
+router.post("/", async (req, res) => {
+  const contactData = req.body;
+});
+
+//Create contacts
+router.post("/", async (req, res) => {
+  const contactData = req.body;
+
+  try {
+    const contact = await ContactModel.create(contactData);
+    res.status(201).json(contact);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json("Bad request");
+  }
+});
+
+// Get contacts by ID
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const contact = await ContactModel.findById(id);
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(400).json("ID not found");
+  }
+});
+
+// Update contact by ID
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const newContactData = req.body;
+
+  try {
+    const contact = await ContactModel.findByIdAndUpdate(id, newContactData, {
+      new: true,
+    });
+    res.status(202).json(contact);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Delete a contact
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const contact = await ContactModel.findByIdAndDelete(id);
+    res.status(200).json("Contact was deleted.");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+require("dotenv").config();
+
+const mongoConfig = require("./config/mongoConfig");
+const contactsRouter = require("./routes/contactsRouter");
 
 const app = express();
 const PORT = 5000;
@@ -7,6 +11,9 @@ app.get("/", (req, res) => {
   res.status(200).json("Welcome to the Contact App.");
 });
 
+app.use("/contacts", contactsRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
+  mongoConfig();
 });
