0aafb5e2adb45facdc2ce5f9c8e9bb5d2b805f1b
Janette Campbell <campbjp@hotmail.com>
2022-06-07 11:45:54 -0400

    added dependencies

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to the Contact App.");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
