a0eb0d98f4158ac28e9c9830b96b366254adfc62
Janette Campbell <campbjp@hotmail.com>
2022-06-10 13:19:33 -0400

    updated Auth routes

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserModel = require("../models/usersSchema");
 const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
 // Create a Router
 const router = express.Router();
@@ -37,27 +38,31 @@ router.post(
     }
 
     try {
+      // Checking if there is a user with this email in the database
+      const userExist = await UserModel.findOne({ email: userData.email });
+
+      //If user exists we return!
+      if (userExist) {
+        return res.json({ msg: "User already exists!" });
+      }
+
+      //* ==== Create new user
+      // Create the salt
+      const SALT = await bcrypt.genSalt(10);
+      // Use the salt to create a hash with the user password
+      const hashedPassword = await bcrypt.hash(userData.password, SALT);
+      // Assign the hashed password to the userData
+      userData.password = hashedPassword;
+
+      // Write the user to the DB
       const user = await UserModel.create(userData);
       res.status(201).json(user);
     } catch (error) {
-      console.error(error);
-      res.status(400).json("Bad request");
+      console.log(error);
+      res.status(400).json("Bad request!!!!!");
     }
   }
 );
-
-// Get user by ID
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const user = await UserModel.findById(id);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json("ID not found");
-  }
-});
-
 // Update user by ID
 router.put("/:id", async (req, res) => {
   const id = req.params.id;
--- server.js
+++ server.js
@@ -6,14 +6,21 @@ const helmet = require("helmet");
 const mongoConfig = require("./config/mongoConfig");
 const usersRouter = require("./routes/usersRouter");
 const contactsRouter = require("./routes/contactsRouter");
-const authRouter = require("./routes/");
+const authRouter = require("./routes/authRouter");
 
 const app = express();
 const PORT = 5000;
 
+//* Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
+
+//* Routers
+
+app.use("/contacts", contactsRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // Create routes
 app.get("/", (req, res) => {
